8db68e8ee96a08abea875f4a6e98744a01e21acc
nikitausicenko <nikita.us@team.anyforsoft.com>
2023-11-11 19:06:43 +0100

    add static server to ios and android, add web build folder

--- App.tsx
+++ App.tsx
@@ -1,118 +1,145 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Dimensions,
+  Platform,
+  ActivityIndicator,
+  StyleSheet,
 } from 'react-native';
+import StaticServer from '@dr.pogodin/react-native-static-server';
+import RNFS from 'react-native-fs';
+import WebView from 'react-native-webview';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Constants
+const SERVER_PORT: number = 9090;
+const ASSETS_FOLDER_NAME: string = 'build';
+const DOCUMENT_FOLDER_PATH: string = `${RNFS.DocumentDirectoryPath}/${ASSETS_FOLDER_NAME}`;
+const LOCALHOST: string =
+  Platform.OS === 'ios' ? RNFS.MainBundlePath : RNFS.DocumentDirectoryPath;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+// Helper function to copy assets
+const copyAssetsFolderContents = async (
+  sourcePath: string,
+  targetPath: string,
+): Promise<void> => {
+  try {
+    const items = await RNFS.readDirAssets(sourcePath);
+    const targetExists = await RNFS.exists(targetPath);
+    if (!targetExists) {
+      await RNFS.mkdir(targetPath);
+    }
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+    for (const item of items) {
+      const sourceItemPath = `${sourcePath}/${item.name}`;
+      const targetItemPath = `${targetPath}/${item.name}`;
+
+      if (item.isDirectory()) {
+        await copyAssetsFolderContents(sourceItemPath, targetItemPath);
+      } else {
+        await RNFS.copyFileAssets(sourceItemPath, targetItemPath);
+      }
+    }
+  } catch (error) {
+    console.error('Failed to copy assets folder contents:', error);
+    throw error;
+  }
+};
+
+function useStaticServer(folderWasCreated: boolean) {
+  const [url, setUrl] = useState<string>('');
+
+  useEffect(() => {
+    let server: StaticServer | null = null;
+
+    const startServer = async (): Promise<void> => {
+      const path: string = `${LOCALHOST}/${ASSETS_FOLDER_NAME}`;
+      server = new StaticServer(SERVER_PORT, path, {localOnly: true});
+
+      try {
+        const url = await server.start();
+        setUrl(url);
+      } catch (error) {
+        console.error('Failed to start server:', error);
+      }
+    };
+
+    if (folderWasCreated) {
+      startServer();
+    }
+
+    return () => server?.stop();
+  }, [folderWasCreated]);
+
+  return url;
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App: React.FC = () => {
+  const [folderWasCreated, setFolderWasCreated] = useState<boolean>(false);
+  const url = useStaticServer(folderWasCreated);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      setFolderWasCreated(true);
+      return;
+    }
+    copyAssetsFolderContents(ASSETS_FOLDER_NAME, DOCUMENT_FOLDER_PATH)
+      .then(() => {
+        console.log('Build folder contents copied successfully.');
+        setFolderWasCreated(true);
+      })
+      .catch(error => {
+        console.error('Error copying build folder contents:', error);
+      });
+  }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar />
+      {url ? (
+        <WebView
+          source={{uri: url}}
+          style={styles.webView}
+          onLoadStart={() => {
+            console.log('WebView started loading.');
+          }}
+          onLoad={() => {
+            console.log('WebView loaded.');
+          }}
+          onError={syntheticEvent => {
+            console.error('WebView error:', syntheticEvent.nativeEvent);
+          }}
+          onHttpError={syntheticEvent => {
+            console.error('HTTP error:', syntheticEvent.nativeEvent);
+          }}
+          javaScriptEnabled={true}
+          originWhitelist={['*']}
+          allowFileAccess={true}
+          debuggingEnabled={true}
+        />
+      ) : (
+        <View style={styles.loaderContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
         </View>
-      </ScrollView>
+      )}
     </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  webView: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  loaderContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
